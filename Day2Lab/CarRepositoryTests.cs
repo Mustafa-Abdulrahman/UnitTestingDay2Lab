60d1f505ec2ea0479e2598ea8d809ecba66ed6d5
Mustafa Abdo <mustafa.abdo4941@gmail.com>
2024-05-16 01:33:59 +0300

    Uploading Files

---
+++ Day2Lab/CarRepositoryTests.cs
@@ -0,0 +1,90 @@
+﻿using CarAPI.Entities;
+
+using CarFactoryAPI.Entities;
+using CarFactoryAPI.Repositories_DAL;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+
+namespace Day2Lab
+{
+	public class CarRepositoryTests
+	{
+		//mock dependencies
+		Mock<FactoryContext> mockFactoryContext;
+
+		//use fake object
+		OwnerRepository _ownerRepository;
+		public CarRepositoryTests()
+		{
+			//mock dependencies
+			mockFactoryContext = new();
+			//use fake object 
+			_ownerRepository = new(mockFactoryContext.Object);
+		}
+
+		[Fact]
+		public void GetAllOwners_AskForOwners_listOfOwners()
+		{
+			//arrange
+			//bulid mock data
+			List<Owner> owners = new List<Owner>()
+			{
+				new Owner(){  Id = 3 ,Name="Mustafa"},
+				new Owner(){  Id = 4 ,Name="Asmaa"},
+				
+			};
+			//setup called Dbsets
+			mockFactoryContext.Setup(o => o.Owners).ReturnsDbSet(owners);
+
+			//act
+			var result = _ownerRepository.GetAllOwners();
+			//assets
+			Assert.NotEmpty(result);
+
+		}
+		[Fact]
+		public void AddOwner_AddingOwner_true()
+		{
+			//arrange
+			//build mock data
+			List<Owner> owners = new List<Owner>()
+			{
+					new Owner(){  Id = 3 ,Name="Mustafa"},
+				new Owner(){  Id = 4 ,Name="Sara"},
+			};
+			Owner owner = new Owner() { Id = 1, Name = "Mustafa" };
+			//setup called Dbsets
+			mockFactoryContext.Setup(o => o.Owners).ReturnsDbSet(owners);
+
+			//act
+			var result = _ownerRepository.AddOwner(owner);
+			//assets
+			Assert.True(result);
+
+		}
+
+		
+		[Fact]
+		public void AddOwner_NotAddingOwnerAlreadyExit_False()
+		{
+			//arrange
+			List<Owner> owners = new List<Owner>()
+			{
+				new Owner(){  Id = 3 ,Name="Mustafa"},
+				new Owner(){  Id = 4 ,Name="Hedra"},
+			};
+			Owner owner = new Owner() { Id = 1, Name = "Ahmed" };
+			//setup called Dbsets
+			mockFactoryContext.Setup(o => o.Owners).ReturnsDbSet(owners);
+
+			//act
+			var result = _ownerRepository.AddOwner(owner);
+			//assets
+			Assert.Equal(4, mockFactoryContext.Object.Owners.Count());
+		}
+
+
+
+	}
+}
---
+++ Day2Lab/OwnersServiceTests.cs
@@ -0,0 +1,116 @@
+﻿using CarAPI.Entities;
+using CarAPI.Models;
+using CarAPI.Payment;
+using CarAPI.Repositories_DAL;
+using CarAPI.Services_BLL;
+using CarFactoryAPI.Entities;
+using CarFactoryAPI.Repositories_DAL;
+using Moq;
+using Moq.EntityFrameworkCore;
+using Xunit.Abstractions;
+
+namespace Day2Lab
+{
+	public class OwnersServiceTests
+	{
+
+		// Create Mock of Dependencies
+		Mock<FactoryContext> contextMock;
+
+
+		CarRepository _carRepository; 
+
+		public void CarRepositoryTests()
+		{
+			// Create Mock of Dependencies
+			contextMock = new();
+
+			// use fake object as dependency
+			_carRepository = new(contextMock.Object);
+		}
+
+
+		[Fact]
+		public void GetCarById_AskById_CarObject()
+		{
+			// Arrange
+			// Build the mock data
+			List<Car> cars = new List<Car>()
+			{
+				new Car() { Id = 2 },
+				new Car() { Id = 4 },
+				new Car() { Id = 6 }
+			};
+
+			// setup called Dbsets
+			contextMock.Setup(o => o.Cars).ReturnsDbSet(cars);
+
+			// Act
+			Car result = _carRepository.GetCarById(4);
+
+			// Assert
+			Assert.NotNull(result);
+		}
+
+	
+	
+		[Fact]
+		public void GetAllCar_AskForCars_CheckIfEmpty()
+		{
+			// Arrange
+			// Build the mock data
+			List<Car> cars = new List<Car>();
+
+			// setup called Dbsets
+			contextMock.Setup(o => o.Cars).ReturnsDbSet(cars);
+
+			// Act
+			var result = _carRepository.GetAllCars();
+
+			// Assert
+			Assert.Empty(result);
+		}
+	
+		
+
+		public void AddCar_CheckIdAddedSuccessfuly_ReturnTrue()
+		{
+			// Arrange
+			
+			var cars = new List<Car>();
+
+			// setup called Dbsets
+			contextMock.Setup(c => c.Cars).ReturnsDbSet(cars);
+			Car car = new Car() { Id = 10,Price=600000,Velocity=30 };
+			// Act
+			bool result = _carRepository.AddCar(car);
+
+			// Assert
+			Assert.True(result);
+		}
+
+		
+		[Fact]
+		public void Remove_CarRemovedSuccessfully()
+		{
+			// Arrange
+		
+
+			// Create a mock car
+			Car car = new Car() { Id = 2 };
+
+			// setup called Dbsets
+			var cars = new List<Car>() { car };
+			contextMock.Setup(c => c.Cars).ReturnsDbSet(cars);
+
+			// Act
+			bool result = _carRepository.Remove(2);
+
+			// Assert
+			Assert.True(result);
+		}
+		
+		
+		
+	}
+}
